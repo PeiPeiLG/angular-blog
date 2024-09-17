d85ebac54fbabc2db6e6987e100008b93389a0fd
EnPei <EnPeiLo@chailease.com.tw>
2024-09-17 12:42:43 +0800

    feat: 程式調整

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
